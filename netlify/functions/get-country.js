79bdcb6f98754855ed2b9df03ba4de833b96e942
Kevin <hello@kevinivan.com>
2024-02-21 14:21:24 +0800

    use x forwarded

--- netlify/functions/get-country.js
+++ netlify/functions/get-country.js
@@ -3,24 +3,24 @@ const GeolocationParams = require('ip-geolocation-api-javascript-sdk/Geolocation
 
 exports.handler = async (event) => {
 	const api = new IPGeolocationAPI(process.env.IP_GEOLOCATION_API_TOKEN, false);
-    const params = new GeolocationParams();
-    const ip = event.headers['client-ip'];
+	const params = new GeolocationParams();
+	const ip = event.headers['x-forwarded-for'];
 
-    params.setIPAddress(ip);
-    params.setFields('country_code2');
+	params.setIPAddress(ip);
+	params.setFields('country_code2');
 
-    try {
-      const result = await new Promise((resolve) => {
-        api.getGeolocation((json) => resolve(json), params);
-      });
-      return {
-				statusCode: 200,
-				headers: {
-					"Access-Control-Allow-Origin": "*",
-					"Access-Control-Allow-Headers": "Content-Type"
-				},
-				body: JSON.stringify(result.country_code2) };
-    } catch (error) {
-      return { statusCode: 500, body: JSON.stringify(error) };
-    }
+	try {
+		const result = await new Promise((resolve) => {
+			api.getGeolocation((json) => resolve(json), params);
+		});
+		return {
+			statusCode: 200,
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Headers": "Content-Type"
+			},
+			body: JSON.stringify(result.country_code2) };
+	} catch (error) {
+		return { statusCode: 500, body: JSON.stringify(error) };
+	}
 };
